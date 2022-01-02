e11d2a405a596151814559245acaf1626f934ff5
anichu <anis.cse5.bu@gmail.com>
2022-01-03 00:36:01 +0600

    backend work for login

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,10 @@ import User from "../models/user.js";
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateAuthToken.js";
 
+// @desc Registered User
+// @route POST /api/users
+// @access Public
+
 export const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 	const existUser = await User.findOne({ email });
@@ -26,5 +30,30 @@ export const registerUser = asyncHandler(async (req, res) => {
 	} else {
 		res.status(400);
 		throw new Error("User not found");
+	}
+});
+
+// @desc Auth User & Get token
+// @route POST /api/users/login
+// @access Public
+
+export const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new Error("User not Found😂😪😥");
+	}
+	const match = await user.matchPassword(password);
+	if (user && match) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password😂🤣");
 	}
 });
